d4a58d1c36313cc90fd8ea149f05971f1cf8704b
Anders Birkenes <datagutten@datagutten.net>
2025-12-19 12:46:52 +0100

    Add correct fallback value

--- config_env.php
+++ config_env.php
@@ -5,6 +5,6 @@ $folders_env = getenv('XMLTV_SUB_FOLDERS');
 if (!empty($folders_env))
     $config['xmltv_sub_folders'] = explode(' ', $folders_env);
 else
-    $config['xmltv_sub_folders'] = '/home/xmltv';
+    $config['xmltv_sub_folders'] = ['xmltv'];
 
 return $config;
