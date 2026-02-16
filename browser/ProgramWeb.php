c91f40c0eecd38c2428cc68e3dc5cdc6fdcf5a41
Anders Birkenes <datagutten@datagutten.net>
2026-02-16 09:21:48 +0100

    Handle programs with no end time

--- browser/ProgramWeb.php
+++ browser/ProgramWeb.php
@@ -6,13 +6,21 @@ use datagutten\xmltv\tools\data\Program;
 use DateTimeZone;
 
 date_default_timezone_set('Europe/Oslo');
+
 class ProgramWeb extends Program
 {
     public function header_web(): string
     {
-        $start=$this->start_obj->setTimezone(new DateTimeZone('Europe/Oslo'));
-        $end=$this->end_obj->setTimezone(new DateTimeZone('Europe/Oslo'));
-        $header = sprintf("%s-%s\n%s", $start->format('H:i'), $end->format('H:i'), $this->title);
+        $start = $this->start_obj->setTimezone(new DateTimeZone('Europe/Oslo'));
+        if (isset($this->end_obj))
+        {
+            $end = $this->end_obj->setTimezone(new DateTimeZone('Europe/Oslo'));
+            $header = sprintf("%s-%s\n%s", $start->format('H:i'), $end->format('H:i'), $this->title);
+        }
+        else
+        {
+            $header = sprintf("%s\n%s", $start->format('H:i'), $this->title);
+        }
         if (!empty($this->episode))
             $header .= ' ' . $this->formatEpisode();
         return $header;
